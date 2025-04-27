e9f877045564be967006696a265a2d4375b220a0
Cousy-1 <alukwecousin@gmail.com>
2025-04-27 18:18:17 +0300

    Used imported components in page layout

---
+++ src/app/portfolio/about.tsx
@@ -0,0 +1,13 @@
+;
+import FactsSidebar from './FactsSidebar';
+
+export default function Page() {
+    return (
+        <div>
+            <FactsSidebar />
+            <h1>Welcome to my page!</h1>
+            <p>This is a simple page with a sidebar.</p>
+            <p>Feel free to explore!</p>
+        </div>
+    );
+}
